247989df43758587ad4cb7f1d15955336373e3a3
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-05 22:35:26 +0000

    Implements the KEY PEGS as a response to user input, game won and game lost messages

--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from copy import deepcopy
 
 class Peg(): 
     def __init__(self, x, y, symbol):
@@ -28,7 +28,7 @@ class CodePeg(Peg):
 
 class KeyPeg(Peg):
     size = 5
-    default = 'X'
+    default = '.'
     def __init__(self, x, y):
         super().__init__(x, y, KeyPeg.default)
 
@@ -41,6 +41,7 @@ class Row():
         self.c_pegs = [CodePeg(x, position) for x in range(pegs, 2 * pegs)]
 
     def draw(self):
+        print('\t\t', end='')
         for kp in self.k_pegs:
             kp.draw()
         print(' | ', end='')
@@ -66,50 +67,80 @@ class Board:
     __code = None
 
     def __init__(self, columns=4, rows=9):
-        self.__columns = columns
-        self.__rows = rows
+        self.__cnt_columns = columns
+        self.__cnt_rows = rows
 
         Row.cnt_pegs = columns
         self.__rows = [Row(idx, columns) for idx in range(rows)]
         self.__code = Row(0, columns)
         """The code to crack"""
         color = None
-        for i in range(self.__columns):
+        for i in range(self.__cnt_columns):
             print(Board.colors)
             while True:
                 color = input("Enter letter for peg #{} Color: ".format(i + 1))
+                color = color.upper()
                 if color in Board.colors.values():
                     break
             self.__code.place(i, color)
-        print("\n<<< The code to Crack >>>")
+        print("\n\t<<< The code to Crack >>>")
         self.__code.draw()
         print('\n\n\n')
 
 
     def draw(self):
+        print('\n\t\tMastermind!')
         for row in self.__rows:
             row.draw()
             print()
+        print()
 
     def nextMove(self):
-        """Human player"""
+        """Gets the player's Input and then process the Computer response."""
+        # Human player
         color = None
-        for i in range(self.__columns):
+        row = self.__rows[Board.atRow]
+        for i in range(self.__cnt_columns):
             print(Board.colors)
             while True:
                 color = input("Enter letter for peg #{} Color: ".format(i + 1))
+                color = color.upper()
                 if color in Board.colors.values():
                     break
-            self.__rows[Board.atRow].place(i, color)
+            row.place(i, color)
+        # Computers Turn
+        cp_row = row.c_pegs
+        k_pegs = row.k_pegs
+        cnt_kpeg = 0
+        CODE = deepcopy(self.__code.c_pegs)
+        # First check those that are the right color at right position
+        for i in range(self.__cnt_columns):
+            if CODE[i].symbol == cp_row[i].symbol:
+                k_pegs[cnt_kpeg].symbol = 'b'
+                cnt_kpeg += 1
+                CODE[i].symbol = None
+        if cnt_kpeg == self.__cnt_columns:
+            Board.finished = True
+            print("\n\t\tGAME WON!  Congratulations!!\n")
+            print("\tCode to Crack ", end='')
+            self.__code.draw()
+            print()
+            return
+        # Now checks for the correct colors but out of position
+        for i in range(self.__cnt_columns):
+            for j in range(self.__cnt_columns):
+                if CODE[i].symbol == cp_row[j].symbol:
+                    k_pegs[cnt_kpeg].symbol = 'w'
+                    cnt_kpeg += 1
+                    CODE[j].symbol = None
+        # Moves the board up to the next row, and check if the end of the game
         Board.atRow += 1
-        """Computer"""
-
-
-
-
-
-
-
+        if Board.atRow == self.__cnt_rows:
+            Board.finished = True
+            print("\n\t\tGame Over!  YOU LOST!!\n")
+            print("\tThe code was: ", end='')
+            self.__code.draw()
+            print('\n')
 
 
 if __name__ == '__main__':
